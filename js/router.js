e30b66e2ac28ee122f278fd18f27ad6f7dcd5b94
joao paulo <joaopaulomoreirarodrigues@gmail.com>
2022-06-12 19:35:04 -0300

    teste 1

--- js/router.js
+++ js/router.js
@@ -1,15 +1,25 @@
 const route = (event) => {
+    //monitora o evento gerado
+
     event = event || window.event;
+    //tira a propriedade padrao dele
     event.preventDefault();
+    //adiciona uma entrada na pilha de historico do navegador
+    //URL para onde a tag âncora aponta
     window.history.pushState({}, "", event.target.href);
+    if((event.target.text == 'Próximo') && window.location.pathname == '/home' && !isValid('#form')){
+        return alert('Preencha os campos em destaque!');
+    }
     handleLocation();
 };
 
 const routes = {
     404: "/pages/404.html",
-    "/": "/pages/index.html",
-    "/about": "/pages/about.html",
-    "/lorem": "/pages/lorem.html",
+    "/": "/pages/login.html",
+    "/home": "/pages/home.html",
+    "/cadastro": "/pages/cadastro.html",
+    "/favoritos": "/pages/favoritos.html",
+    "/senha": "/pages/senha.html",
 };
 
 const handleLocation = async () => {
@@ -23,3 +33,4 @@ window.onpopstate = handleLocation;
 window.route = route;
 
 handleLocation();
+
